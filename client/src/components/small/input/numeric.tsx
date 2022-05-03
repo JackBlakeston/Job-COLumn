db5b701b1b0c34e0af84c2deb2b562f65c59b0b1
Fav8 <leonardo.damico7597@gmail.com>
2022-05-03 21:41:01 +0200

    ts numeric

--- client/src/components/small/input/numeric.tsx
+++ client/src/components/small/input/numeric.tsx
@@ -4,22 +4,29 @@ import { NumericInput } from '@blueprintjs/core';
 // Local imports
 import GBP from '../GBP/GBP';
 
+type props = {
+  defaultValue: any,
+  fill: boolean,
+  onValueChange: any,
+  placeholder: string
+}
+
 function Numeric ({
   defaultValue,
   fill,
   onValueChange,
   placeholder
-}) {
+}: props) : JSX.Element {
   return (
     <NumericInput
       defaultValue={defaultValue}
       fill={fill}
       leftIcon={<GBP />}
-      majorStepSize='10000'
+      majorStepSize= {10000} //worked with strings without js, didnt like them in ts, will se if this breaks everything, if so .tostring()
       min={0}
       onValueChange={onValueChange}
       placeholder={placeholder}
-      stepSize='1000'
+      stepSize= {1000} //worked with strings without js, didnt like them in ts, will se if this breaks everything, if so .tostring()
     />
   )
 }
