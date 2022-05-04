f4b19ea479660798aa028fbf00a5367a9c69719d
Jack Blakeston <jackblakeston93@gmail.com>
2022-05-04 02:05:32 +0200

    Minorissimo: move a line

--- client/src/components/helpers/sidebar/sortDefined.tsx
+++ client/src/components/helpers/sidebar/sortDefined.tsx
@@ -1,10 +1,10 @@
 import { ActionProps, H5, Icon } from '@blueprintjs/core';
+import { BlueprintIcons_16Id } from '@blueprintjs/icons/lib/esm/generated/16px/blueprint-icons-16';
 
 import Sorts from '../../small/input/sorts';
 import PrimaryButton from '../../small/buttons/primaryButton';
 import SecondaryButton from '../../small/buttons/secondaryButton';
 import css from '../../contexts/themes.scss';
-import { BlueprintIcons_16Id } from '@blueprintjs/icons/lib/esm/generated/16px/blueprint-icons-16';
 
 type sortDefinedProps = {
   sortOrder: string,
